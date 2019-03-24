0d541ab2efdcca7f820fdda1f0337938efd2367a
huanghong <350569321@qq.com>
2019-03-24 13:28:21 +0800

    springboot学习

--- day1/src/main/java/com/springboot/oneday/Controller/HelloController.java
+++ day1/src/main/java/com/springboot/oneday/Controller/HelloController.java
@@ -3,6 +3,10 @@ package com.springboot.oneday.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 控制器
+ * @author: huangh
+ */
 @RestController
 @RequestMapping
 public class HelloController {
